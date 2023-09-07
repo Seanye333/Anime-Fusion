3b90656c7546ee67f5b6922852d4e979654999ae
Joseph DeMasse <demasse1125@gmail.com>
2023-09-06 20:35:45 -0400

    initial commit'

---
+++ .history/db/seeds_20230906203301.sql
@@ -0,0 +1,25 @@
+
+INSERT INTO Anime (title, genre, episodes, release_year) VALUES
+    ('Naruto', 'Action, Adventure', 220, 2002),
+    ('One Piece', 'Action, Adventure', 1044, 1999),
+    ('Attack on Titan', 'Action, Fantasy', 75, 2013),
+    ('My Hero Academia', 'Action, Superhero', 113, 2016);
+    ('Death Note', 'Mystery, Psychological', 37, 2006),
+    ('Fullmetal Alchemist: Brotherhood', 'Action, Adventure', 64, 2009),
+    ('Demon Slayer: Kimetsu no Yaiba', 'Action, Fantasy', 26, 2019),
+    ('One Punch Man', 'Action, Comedy', 24, 2015);
+
+
+INSERT INTO Character (name, anime_id) VALUES
+    ('Naruto Uzumaki', 1),
+    ('Monkey D. Luffy', 2),
+    ('Eren Yeager', 3),
+    ('Izuku Midoriya', 4);
+    ('Light Yagami', 5),
+    ('L', 5),
+    ('Edward Elric', 6),
+    ('Alphonse Elric', 6),
+    ('Tanjiro Kamado', 7),
+    ('Nezuko Kamado', 7),
+    ('Saitama', 8),
+    ('Genos', 8);
