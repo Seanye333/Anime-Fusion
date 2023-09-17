c7d61fc1b688f18aeec8faf086849be240ca755c
Cameron Morin <morinc35@gmail.com>
2023-09-17 09:44:55 -0400

    model fixes

--- models/Anime.js
+++ models/Anime.js
@@ -1,20 +1,28 @@
-const { DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const User = require('./User');
-const UsersAnime = require('./UsersAnime');
 
-const Anime = sequelize.define('animes', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-    unique: true
+class Anime extends Model {}
+
+Anime.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+      unique: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    modelName: 'animes'
   }
-});
+);
 
 module.exports = Anime;
--- models/UsersAnime.js
+++ models/UsersAnime.js
@@ -1,32 +1,42 @@
-const { DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const Anime = require('./Anime');
 const User = require('./User');
 
-const UsersAnime = sequelize.define('users_anime', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: User,
-      key: 'id'
+class UsersAnime extends Model {}
+
+UsersAnime.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: 'id'
+      }
+    },
+    anime_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Anime,
+        key: 'id'
+      }
+    },
+    watchlist: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
     }
   },
-  anime_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: Anime,
-      key: 'id'
-    }
-  },
-  watchlist: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    modelName: 'users_anime'
   }
-});
+);
 
 module.exports = UsersAnime;
